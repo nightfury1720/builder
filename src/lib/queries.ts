3476a7cb025926aa4901f7e4ff33f2816c9a92b1
Suraj <skd18@iitbbs.ac.in>
2024-04-02 12:30:03 +0530

    feat: sidebar

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -14,7 +14,7 @@ import {
   Ticket,
   User,
 } from "@prisma/client";
-// import { v4 } from 'uuid'
+import { v4 } from "uuid";
 
 // import {
 //   CreateFunnelFormSchema,
@@ -91,7 +91,7 @@ export const saveActivityLogsNotification = async ({
   if (!foundAgencyId) {
     if (!subaccountId) {
       throw new Error(
-        "You need to provide atleast an agency Id or subaccount Id",
+        "You need to provide atleast an agency Id or subaccount Id"
       );
     }
     const response = await db.subAccount.findUnique({
@@ -195,7 +195,7 @@ export const verifyAndAcceptInvitation = async () => {
 
 export const updateAgencyDetails = async (
   agencyId: string,
-  agencyDetails: Partial<Agency>,
+  agencyDetails: Partial<Agency>
 ) => {
   const response = await db.agency.update({
     where: { id: agencyId },
@@ -307,85 +307,85 @@ export const getNotificationAndUser = async (agencyId: string) => {
   }
 };
 
-// export const upsertSubAccount = async (subAccount: SubAccount) => {
-//   if (!subAccount.companyEmail) return null
-//   const agencyOwner = await db.user.findFirst({
-//     where: {
-//       Agency: {
-//         id: subAccount.agencyId,
-//       },
-//       role: 'AGENCY_OWNER',
-//     },
-//   })
-//   if (!agencyOwner) return console.log('🔴Erorr could not create subaccount')
-//   const permissionId = v4()
-//   const response = await db.subAccount.upsert({
-//     where: { id: subAccount.id },
-//     update: subAccount,
-//     create: {
-//       ...subAccount,
-//       Permissions: {
-//         create: {
-//           access: true,
-//           email: agencyOwner.email,
-//           id: permissionId,
-//         },
-//         connect: {
-//           subAccountId: subAccount.id,
-//           id: permissionId,
-//         },
-//       },
-//       Pipeline: {
-//         create: { name: 'Lead Cycle' },
-//       },
-//       SidebarOption: {
-//         create: [
-//           {
-//             name: 'Launchpad',
-//             icon: 'clipboardIcon',
-//             link: `/subaccount/${subAccount.id}/launchpad`,
-//           },
-//           {
-//             name: 'Settings',
-//             icon: 'settings',
-//             link: `/subaccount/${subAccount.id}/settings`,
-//           },
-//           {
-//             name: 'Funnels',
-//             icon: 'pipelines',
-//             link: `/subaccount/${subAccount.id}/funnels`,
-//           },
-//           {
-//             name: 'Media',
-//             icon: 'database',
-//             link: `/subaccount/${subAccount.id}/media`,
-//           },
-//           {
-//             name: 'Automations',
-//             icon: 'chip',
-//             link: `/subaccount/${subAccount.id}/automations`,
-//           },
-//           {
-//             name: 'Pipelines',
-//             icon: 'flag',
-//             link: `/subaccount/${subAccount.id}/pipelines`,
-//           },
-//           {
-//             name: 'Contacts',
-//             icon: 'person',
-//             link: `/subaccount/${subAccount.id}/contacts`,
-//           },
-//           {
-//             name: 'Dashboard',
-//             icon: 'category',
-//             link: `/subaccount/${subAccount.id}`,
-//           },
-//         ],
-//       },
-//     },
-//   })
-//   return response
-// }
+export const upsertSubAccount = async (subAccount: SubAccount) => {
+  if (!subAccount.companyEmail) return null;
+  const agencyOwner = await db.user.findFirst({
+    where: {
+      Agency: {
+        id: subAccount.agencyId,
+      },
+      role: "AGENCY_OWNER",
+    },
+  });
+  if (!agencyOwner) return console.log("🔴Erorr could not create subaccount");
+  const permissionId = v4();
+  const response = await db.subAccount.upsert({
+    where: { id: subAccount.id },
+    update: subAccount,
+    create: {
+      ...subAccount,
+      Permissions: {
+        create: {
+          access: true,
+          email: agencyOwner.email,
+          id: permissionId,
+        },
+        connect: {
+          subAccountId: subAccount.id,
+          id: permissionId,
+        },
+      },
+      Pipeline: {
+        create: { name: "Lead Cycle" },
+      },
+      SidebarOption: {
+        create: [
+          {
+            name: "Launchpad",
+            icon: "clipboardIcon",
+            link: `/subaccount/${subAccount.id}/launchpad`,
+          },
+          {
+            name: "Settings",
+            icon: "settings",
+            link: `/subaccount/${subAccount.id}/settings`,
+          },
+          {
+            name: "Funnels",
+            icon: "pipelines",
+            link: `/subaccount/${subAccount.id}/funnels`,
+          },
+          {
+            name: "Media",
+            icon: "database",
+            link: `/subaccount/${subAccount.id}/media`,
+          },
+          {
+            name: "Automations",
+            icon: "chip",
+            link: `/subaccount/${subAccount.id}/automations`,
+          },
+          {
+            name: "Pipelines",
+            icon: "flag",
+            link: `/subaccount/${subAccount.id}/pipelines`,
+          },
+          {
+            name: "Contacts",
+            icon: "person",
+            link: `/subaccount/${subAccount.id}/contacts`,
+          },
+          {
+            name: "Dashboard",
+            icon: "category",
+            link: `/subaccount/${subAccount.id}`,
+          },
+        ],
+      },
+    },
+  });
+  return response;
+};
 
 // export const getUserPermissions = async (userId: string) => {
 //   const response = await db.user.findUnique({
